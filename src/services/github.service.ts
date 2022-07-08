ab4ba3fb4a075d0e4eedbbf090ca7a8eeaa321bf
fthomasvp <fellipecab@gmail.com>
2022-07-07 21:35:31 -0300

    refactor: remove unnecessary file

--- src/services/github.service.ts
+++ src/services/github.service.ts
@@ -1,7 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 import type { Repo } from "../features/github/github.types";
-import type { BaseQueryParams } from "./service.types";
+
+type BaseQueryParams = {
+  /** Can be one of: `all`, `owner`, `member` */
+  type: "all" | "owner" | "member";
+
+  /** Default: 1 */
+  page: number;
+
+  /** Default: 30 */
+  per_page: number;
+
+  /** Can be one of: `created`, `updated`, `pushed`, `full_name`.
+  Default: `full_name` */
+  sort: "created " | "updated " | "pushed" | "full_name";
+};
 
 const DEFAULT_PARAMS: Partial<BaseQueryParams> = {
   page: 1,
